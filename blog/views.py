bd171d3f135bff02e5e3b6a4e2a5ec7fb5d2545c
Artem Jasan <artjasan@gmail.com>
2020-02-13 13:33:37 +0100

    Adding a new Class, update HTML files

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import ListView
 
 from .models import Post
 
+
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # Only 3 articles on the page.
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If the page does not int number, return the first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if the page number more than quantity of pages, return the lats page. 
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page':page,'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',publish__year=year, publish__month=month, publish__day=day)
     return render(request,'blog/post/detail.html', {'post': post})    
 
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
  
