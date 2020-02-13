89f4ae08978222afec2d2c7dcc63a289308f2c78
Artem Jasan <artjasan@gmail.com>
2020-02-13 16:45:39 +0100

    Email sending function

--- Site/settings.py
+++ Site/settings.py
@@ -119,3 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_HOST_USER = 'name@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'password'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
 
+
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -31,4 +33,23 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
- 
+
+def post_share(request, post_id):
+    # Getting an article by ID
+    post = get_object_or_404(Post, id=post_id,status='published')
+    sent = False
+    if request.method == 'POST':
+        # Form send to save.
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+        # All fields have been validated.
+            cd = form.cleaned_data
+        # Mail drop-off
+        post_url = request.build_absolute_uri(post.get_absolute_url())
+        subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+        message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'], cd['comments'])
+        send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+        sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent':sent})
